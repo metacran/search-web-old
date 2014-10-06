ad1f3b5fca55c9d7c8abc576a19d19b28887cadf
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-06 17:07:00 -0400

    Various fixes
    * Use R logo
    * Larger title on main page
    * Placeholder in search box
    * Example searches
    * Correct time-ago for recently updated packages

--- js/cran_top.js
+++ js/cran_top.js
@@ -1,12 +1,32 @@
 
+function short_date(date) {
+    if (moment(date) < moment()) {
+	return moment(date)
+	    .fromNow()
+	    .replace("seconds ago", "1m")
+	    .replace("a few seconds", "1m")
+	    .replace("a minute ago", "1m")
+	    .replace(" minutes ago", "m")
+	    .replace("an hour ago", "1h")
+	    .replace(" hours ago", "h")
+	    .replace("a day ago", "1d")
+	    .replace(" days ago", "d")
+	    .replace("a month ago", "1M")
+	    .replace(" months ago", "M")
+	    .replace("a year ago", "1y")
+	    .replace(" years ago", "y")
+    } else {
+	return "1s";
+    }
+}
+
 function add_recent(data) {
     var parent = document.getElementById("recently-updated");
     var text = "<ul>";
     for (var pkg in data) {
 	var name = data[pkg].name;
-	var date = moment(data[pkg].date);
-	var fdate = date.format("ddd MMM DD YYYY hh:mm:ss [UTC]");
-	text += '<li>' + timeago(fdate) +
+	var date = short_date(data[pkg].date);
+	text += '<li>' + date +
 	    '<a href="http://cran.r-project.org/package=' +
 	    name + '"> ' + name + '</a></li>';
     }
